71acba76b3201d6ed5321ad73267715d8925c72e
Petr Neduchal <neduchal@gmail.com>
2025-01-06 12:44:48 +0100

    yolox_s exps file

---
+++ exps/cars_dataset/yolox_s.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+import os
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 0.33
+        self.width = 0.50
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+
+        self.data_dir = "datasets/CarsDataset"
+        self.train_ann = "instances_train.json"
+        self.val_ann = "instances_valid.json"
+        self.test_ann = "instances_test.json"
+
+        self.data_num_workers = 1
+        self.max_epoch = 10
+
+    def get_dataset(self, cache: bool = False, cache_type: str = "ram"):
+        """
+        Get dataset according to cache and cache_type parameters.
+        Args:
+            cache (bool): Whether to cache imgs to ram or disk.
+            cache_type (str, optional): Defaults to "ram".
+                "ram" : Caching imgs to ram for fast training.
+                "disk": Caching imgs to disk for fast training.
+        """
+        from yolox.data import CarsDataset, TrainTransform
+
+        return CarsDataset(
+            data_dir=self.data_dir,
+            json_file=self.train_ann,
+            img_size=self.input_size,
+            preproc=TrainTransform(
+                max_labels=50,
+                flip_prob=self.flip_prob,
+                hsv_prob=self.hsv_prob
+            ),
+            cache=cache,
+            cache_type=cache_type,
+        )
